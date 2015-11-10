66c07383ca10eb47669b592f9f9b9360a9ba6988
Murad <mrzeeshank@gmail.com>
2015-11-10 10:32:16 -0800

    init commit

---
+++ RemoveDuplicatesFromArray.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+	{
+	int arr[] = {1, 5, 1, 4, 5, 9 ,20};
+	int size = 7, newSize = 0;
+
+	for(int i = 0; i < size -1 ; i++)
+		{
+		cout << arr[i] << " " ;
+		}
+	for(int i = 0; i< size -1 ; i++)
+		{
+			if(arr[i] == arr[i+1])
+				{
+					
+				}
+		}
+
+	for(int i = 0; i < size -1 ; i++)
+		{
+		cout << arr[i] << " " ;
+		}
+
+	return 0;
+	}
